9c07d440ddb05b7f5e4e655e7c8ee261deaf29ae
Inyoung Hong <ih235@cornell.edu>
2017-05-05 15:49:47 -0700

    new display changes

--- resources/profile_greeting/user_greetings/display.php
+++ resources/profile_greeting/user_greetings/display.php
@@ -5,11 +5,11 @@ $visitor = '';
 $new_message1 = str_replace('visitor', $visitor, 'Hello visitor! Welcome to my page!');
 $new_message2 = str_replace('visitor', $visitor, 'Please make yourself at home!');
 
-$text_box1 = imagettfbbox(41, 0, '../uploaded_fonts/brain_flower.ttf', $new_message1);
+$text_box1 = imagettfbbox(43, 0, '../uploaded_fonts/brain_flower.ttf', $new_message1);
 $text_width1 = $text_box1[2] - $text_box1[0];
 $full_width1 = $text_width1 + (14 * 2);
 
-$text_box2 = imagettfbbox(41, 0, '../uploaded_fonts/brain_flower.ttf', $new_message2);
+$text_box2 = imagettfbbox(43, 0, '../uploaded_fonts/brain_flower.ttf', $new_message2);
 $text_width2 = $text_box2[2] - $text_box2[0];
 $full_width2 = $text_width2 + (14 * 2);
 
@@ -25,9 +25,9 @@ if ($full_width1 >= $full_width2) {
     $x1 = ($width - $text_width1) / 2;
 }
 
-$y1 = 20 + 41;
+$y1 = 20 + 43;
 $y2 = $y1 + 8 + $text_height;
-$height = $y2 + 20 + 41/2;
+$height = $y2 + 20 + 43/2;
 
 $im = imagecreatetruecolor ($width, $height);
 imagealphablending($im, true);
@@ -35,8 +35,8 @@ imagesavealpha($im, true);
 $background = imagecolorallocate($im, 0, 0, 0);
 imagefill($im,0,0,0x7fff0000);
 $color = imagecolorallocate($im, 73, 31, 181);
-imagettftext($im, 41, 0, $x1, $y1, $color, '../uploaded_fonts/brain_flower.ttf', $new_message1);
-imagettftext($im, 41, 0, $x2, $y2, $color, '../uploaded_fonts/brain_flower.ttf', $new_message2);
+imagettftext($im, 43, 0, $x1, $y1, $color, '../uploaded_fonts/brain_flower.ttf', $new_message1);
+imagettftext($im, 43, 0, $x2, $y2, $color, '../uploaded_fonts/brain_flower.ttf', $new_message2);
 imagepng($im);
 imagedestroy($im);
 ?>
