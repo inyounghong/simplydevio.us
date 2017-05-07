5a43b58676f6574e1c0e7039633526bd597b21f5
Inyoung Hong <ih235@cornell.edu>
2017-05-06 19:41:03 -0700

    new greeting

--- resources/profile_greeting/user_greetings/foxytales-20170506.php
+++ resources/profile_greeting/user_greetings/foxytales-20170506.php
@@ -1,15 +1,15 @@
 <?
 session_start();
 header('Content-type: image/png');
-$visitor = 'Porsheee';
+$visitor = exec('python ../python/image.py foxytales visitor');
 $new_message1 = str_replace('visitor', $visitor, 'Hey visitor!');
-$new_message2 = str_replace('visitor', $visitor, 'Welcome to my page!');
+$new_message2 = str_replace('visitor', $visitor, 'Welcome to the Foxytales!');
 
-$text_box1 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message1);
+$text_box1 = imagettfbbox(50, 0, '../uploaded_fonts/brain_flower.ttf', $new_message1);
 $text_width1 = $text_box1[2] - $text_box1[0];
 $full_width1 = $text_width1 + (20 * 2);
 
-$text_box2 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message2);
+$text_box2 = imagettfbbox(50, 0, '../uploaded_fonts/brain_flower.ttf', $new_message2);
 $text_width2 = $text_box2[2] - $text_box2[0];
 $full_width2 = $text_width2 + (20 * 2);
 
@@ -25,18 +25,18 @@ if ($full_width1 >= $full_width2) {
     $x1 = ($width - $text_width1) / 2;
 }
 
-$y1 = 20 + 15;
+$y1 = 20 + 50;
 $y2 = $y1 + 10 + $text_height;
-$height = $y2 + 20 + 15/2;
+$height = $y2 + 20 + 50/2;
 
 $im = imagecreatetruecolor ($width, $height);
 imagealphablending($im, true);
 imagesavealpha($im, true);
 $background = imagecolorallocate($im, 0, 0, 0);
-imagefill($im,0,0,$background);
-$color = imagecolorallocate($im, 255, 255, 255);
-imagettftext($im, 15, 0, $x1, $y1, $color, '../uploaded_fonts/arberkley.ttf', $new_message1);
-imagettftext($im, 15, 0, $x2, $y2, $color, '../uploaded_fonts/arberkley.ttf', $new_message2);
+imagefill($im,0,0,0x7fff0000);
+$color = imagecolorallocate($im, 74, 0, 0);
+imagettftext($im, 50, 0, $x1, $y1, $color, '../uploaded_fonts/brain_flower.ttf', $new_message1);
+imagettftext($im, 50, 0, $x2, $y2, $color, '../uploaded_fonts/brain_flower.ttf', $new_message2);
 imagepng($im);
 imagedestroy($im);
 ?>
