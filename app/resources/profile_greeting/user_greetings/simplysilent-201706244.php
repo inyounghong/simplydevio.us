13665f7a5d80f161215b203e3ad218ce30bc692e
inyounghong <ih235@cornell.edu>
2017-06-24 22:23:39 -0400

    display

--- app/resources/profile_greeting/user_greetings/simplysilent-201706244.php
+++ app/resources/profile_greeting/user_greetings/simplysilent-201706244.php
@@ -2,6 +2,7 @@
 $testGD = get_extension_funcs("gd"); // Grab function list
 if (!$testGD){ echo "GD not even installed."; exit; }
 echo"<pre>".print_r($testGD,true)."</pre>";
+echo phpInfo();
 
 
 $png = imagecreatetruecolor(800, 600);
