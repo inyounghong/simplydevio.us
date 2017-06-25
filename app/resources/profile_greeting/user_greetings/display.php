aaf09e920f57ba6d1e41ce9a1723677b1834657d
inyounghong <ih235@cornell.edu>
2017-06-24 20:16:22 -0400

    display

--- app/resources/profile_greeting/user_greetings/display.php
+++ app/resources/profile_greeting/user_greetings/display.php
@@ -1,33 +1,34 @@
 <?
 session_start();
 // header('Content-type: image/png');
-$visitor = 'KlodHopper';
-$new_message1 = str_replace('visitor', $visitor, 'Hey visitor!');
-$new_message2 = str_replace('visitor', $visitor, 'Welcome to my page!');
-
-$text_box1 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message1);
-$text_width1 = $text_box1[2] - $text_box1[0];
-$full_width1 = $text_width1 + (20 * 2);
-
-$text_box2 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message2);
-$text_width2 = $text_box2[2] - $text_box2[0];
-$full_width2 = $text_width2 + (20 * 2);
-
-$text_height = $text_box2[1] - $text_box2[5];
-
-if ($full_width1 >= $full_width2) {
-    $width = $full_width1;
-    $x1 = 20;
-    $x2 = ($width - $text_width2)/2;
-} else {
-    $width = $full_width2;
-    $x2 = 20;
-    $x1 = ($width - $text_width1) / 2;
-}
-
-$y1 = 20 + 15;
-$y2 = $y1 + 10 + $text_height;
-$height = $y2 + 20 + 15/2;
+// $visitor = 'KlodHopper';
+// $new_message1 = str_replace('visitor', $visitor, 'Hey visitor!');
+// $new_message2 = str_replace('visitor', $visitor, 'Welcome to my page!');
+//
+// $text_box1 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message1);
+// $text_width1 = $text_box1[2] - $text_box1[0];
+// $full_width1 = $text_width1 + (20 * 2);
+//
+// $text_box2 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message2);
+// $text_width2 = $text_box2[2] - $text_box2[0];
+// $full_width2 = $text_width2 + (20 * 2);
+//
+// $text_height = $text_box2[1] - $text_box2[5];
+//
+// if ($full_width1 >= $full_width2) {
+//     $width = $full_width1;
+//     $x1 = 20;
+//     $x2 = ($width - $text_width2)/2;
+// } else {
+//     $width = $full_width2;
+//     $x2 = 20;
+//     $x1 = ($width - $text_width1) / 2;
+// }
+//
+// $y1 = 20 + 15;
+// $y2 = $y1 + 10 + $text_height;
+// $height = $y2 + 20 + 15/2;
+echo "display";
 
 // $im = imagecreatetruecolor ($width, $height);
 // imagealphablending($im, true);
