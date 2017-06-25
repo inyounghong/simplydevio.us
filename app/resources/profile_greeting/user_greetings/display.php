4315da4c580fc96a3adc77c44740f5bca059d294
inyounghong <ih235@cornell.edu>
2017-06-24 20:09:54 -0400

    json

--- app/resources/profile_greeting/user_greetings/display.php
+++ app/resources/profile_greeting/user_greetings/display.php
@@ -1,19 +1,15 @@
 <?
 session_start();
 header('Content-type: image/png');
-<<<<<<< HEAD
 $visitor = 'KlodHopper';
-=======
-$visitor = 'Shafttt';
->>>>>>> 3a9c20aed4bbe3af5d2c639688c2520e02d6a532
 $new_message1 = str_replace('visitor', $visitor, 'Hey visitor!');
 $new_message2 = str_replace('visitor', $visitor, 'Welcome to my page!');
 
-$text_box1 = imagettfbbox(26, 0, '../uploaded_fonts/arberkley.ttf', $new_message1);
+$text_box1 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message1);
 $text_width1 = $text_box1[2] - $text_box1[0];
 $full_width1 = $text_width1 + (20 * 2);
 
-$text_box2 = imagettfbbox(26, 0, '../uploaded_fonts/arberkley.ttf', $new_message2);
+$text_box2 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message2);
 $text_width2 = $text_box2[2] - $text_box2[0];
 $full_width2 = $text_width2 + (20 * 2);
 
@@ -29,9 +25,9 @@ if ($full_width1 >= $full_width2) {
     $x1 = ($width - $text_width1) / 2;
 }
 
-$y1 = 20 + 26;
+$y1 = 20 + 15;
 $y2 = $y1 + 10 + $text_height;
-$height = $y2 + 20 + 26/2;
+$height = $y2 + 20 + 15/2;
 
 $im = imagecreatetruecolor ($width, $height);
 imagealphablending($im, true);
@@ -39,8 +35,8 @@ imagesavealpha($im, true);
 $background = imagecolorallocate($im, 0, 0, 0);
 imagefill($im,0,0,$background);
 $color = imagecolorallocate($im, 255, 255, 255);
-imagettftext($im, 26, 0, $x1, $y1, $color, '../uploaded_fonts/arberkley.ttf', $new_message1);
-imagettftext($im, 26, 0, $x2, $y2, $color, '../uploaded_fonts/arberkley.ttf', $new_message2);
+imagettftext($im, 15, 0, $x1, $y1, $color, '../uploaded_fonts/arberkley.ttf', $new_message1);
+imagettftext($im, 15, 0, $x2, $y2, $color, '../uploaded_fonts/arberkley.ttf', $new_message2);
 imagepng($im);
 imagedestroy($im);
 ?>
