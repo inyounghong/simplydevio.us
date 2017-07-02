a8494a0e2fd1c83e74bf70799fa8b316ac8d1d48
inyounghong <ih235@cornell.edu>
2017-07-02 11:56:17 -0700

    display

--- app/resources/profile_greeting/user_greetings/display.php
+++ app/resources/profile_greeting/user_greetings/display.php
@@ -1,15 +1,14 @@
 <?
-session_start();
 header('Content-type: image/png');
-$visitor = 'Ango-the-Angolion';
-$new_message1 = str_replace('visitor', $visitor, 'Hello there, visitor! I can see you!');
-$new_message2 = str_replace('visitor', $visitor, 'Welcome to my profile!');
+$visitor = 'DustMoonFeather';
+$new_message1 = str_replace('visitor', $visitor, 'Hey visitor!');
+$new_message2 = str_replace('visitor', $visitor, 'Welcome to my page!');
 
-$text_box1 = imagettfbbox(16, 0, '../uploaded_fonts/arial.ttf', $new_message1);
+$text_box1 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message1);
 $text_width1 = $text_box1[2] - $text_box1[0];
 $full_width1 = $text_width1 + (20 * 2);
 
-$text_box2 = imagettfbbox(16, 0, '../uploaded_fonts/arial.ttf', $new_message2);
+$text_box2 = imagettfbbox(15, 0, '../uploaded_fonts/arberkley.ttf', $new_message2);
 $text_width2 = $text_box2[2] - $text_box2[0];
 $full_width2 = $text_width2 + (20 * 2);
 
@@ -25,18 +24,18 @@ if ($full_width1 >= $full_width2) {
     $x1 = ($width - $text_width1) / 2;
 }
 
-$y1 = 20 + 16;
+$y1 = 20 + 15;
 $y2 = $y1 + 10 + $text_height;
-$height = $y2 + 20 + 16/2;
+$height = $y2 + 20 + 15/2;
 
 $im = imagecreatetruecolor ($width, $height);
 imagealphablending($im, true);
 imagesavealpha($im, true);
 $background = imagecolorallocate($im, 0, 0, 0);
-imagefill($im,0,0,0x7fff0000);
-$color = imagecolorallocate($im, 0, 0, 0);
-imagettftext($im, 16, 0, $x1, $y1, $color, '../uploaded_fonts/arial.ttf', $new_message1);
-imagettftext($im, 16, 0, $x2, $y2, $color, '../uploaded_fonts/arial.ttf', $new_message2);
+imagefill($im,0,0,$background);
+$color = imagecolorallocate($im, 255, 255, 255);
+imagettftext($im, 15, 0, $x1, $y1, $color, '../uploaded_fonts/arberkley.ttf', $new_message1);
+imagettftext($im, 15, 0, $x2, $y2, $color, '../uploaded_fonts/arberkley.ttf', $new_message2);
 imagepng($im);
 imagedestroy($im);
 ?>
