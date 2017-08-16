417692dfe37602dd666d21515d5315e36959ead1
inyounghong <ih235@cornell.edu>
2017-08-16 00:00:02 -0700

    “Push”

---
+++ app/resources/profile_greeting/user_greetings/caldercloud-20170816.php
@@ -0,0 +1,43 @@
+<?
+header('Content-type: image/png');
+include('../php/VisitorScraper.php');
+$visitorScraper = new VisitorScraper("caldercloud", false);
+$visitor = $visitorScraper->getVisitor();
+$new_message1 = str_replace('visitor', $visitor, 'Hey there friend!');
+$new_message2 = str_replace('visitor', $visitor, 'Welcome to my page!');
+
+$text_box1 = imagettfbbox(33, 0, '../uploaded_fonts/monotypecorsiva.ttf', $new_message1);
+$text_width1 = $text_box1[2] - $text_box1[0];
+$full_width1 = $text_width1 + (21 * 2);
+
+$text_box2 = imagettfbbox(33, 0, '../uploaded_fonts/monotypecorsiva.ttf', $new_message2);
+$text_width2 = $text_box2[2] - $text_box2[0];
+$full_width2 = $text_width2 + (21 * 2);
+
+$text_height = $text_box2[1] - $text_box2[5];
+
+if ($full_width1 >= $full_width2) {
+    $width = $full_width1;
+    $x1 = 21;
+    $x2 = ($width - $text_width2)/2;
+} else {
+    $width = $full_width2;
+    $x2 = 21;
+    $x1 = ($width - $text_width1) / 2;
+}
+
+$y1 = 20 + 33;
+$y2 = $y1 + 10 + $text_height;
+$height = $y2 + 20 + 33/2;
+
+$im = imagecreatetruecolor ($width, $height);
+imagealphablending($im, true);
+imagesavealpha($im, true);
+$background = imagecolorallocate($im, 0, 0, 0);
+imagefill($im,0,0,0x7fff0000);
+$color = imagecolorallocate($im, 255, 255, 255);
+imagettftext($im, 33, 0, $x1, $y1, $color, '../uploaded_fonts/monotypecorsiva.ttf', $new_message1);
+imagettftext($im, 33, 0, $x2, $y2, $color, '../uploaded_fonts/monotypecorsiva.ttf', $new_message2);
+imagepng($im);
+imagedestroy($im);
+?>
