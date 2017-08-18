8e441fc066bec9b5812a32216fa257ee9e129480
inyounghong <ih235@cornell.edu>
2017-08-18 12:00:02 -0700

    “Push”

--- app/resources/profile_greeting/user_greetings/bad-squirrell-20170818.php
+++ app/resources/profile_greeting/user_greetings/bad-squirrell-20170818.php
@@ -6,29 +6,29 @@ $visitor = $visitorScraper->getVisitor();
 $new_message1 = str_replace('visitor', $visitor, 'Hey visitor!');
 $new_message2 = str_replace('visitor', $visitor, 'Thanks for having a look at my page!');
 
-$text_box1 = imagettfbbox(20, 0, '../uploaded_fonts/kgsevensixteen.ttf', $new_message1);
+$text_box1 = imagettfbbox(22, 0, '../uploaded_fonts/comingsoon.ttf', $new_message1);
 $text_width1 = $text_box1[2] - $text_box1[0];
-$full_width1 = $text_width1 + (18 * 2);
+$full_width1 = $text_width1 + (0 * 2);
 
-$text_box2 = imagettfbbox(20, 0, '../uploaded_fonts/kgsevensixteen.ttf', $new_message2);
+$text_box2 = imagettfbbox(22, 0, '../uploaded_fonts/comingsoon.ttf', $new_message2);
 $text_width2 = $text_box2[2] - $text_box2[0];
-$full_width2 = $text_width2 + (18 * 2);
+$full_width2 = $text_width2 + (0 * 2);
 
 $text_height = $text_box2[1] - $text_box2[5];
 
 if ($full_width1 >= $full_width2) {
     $width = $full_width1;
-    $x1 = 18;
+    $x1 = 0;
     $x2 = ($width - $text_width2)/2;
 } else {
     $width = $full_width2;
-    $x2 = 18;
+    $x2 = 0;
     $x1 = ($width - $text_width1) / 2;
 }
 
-$y1 = 20 + 20;
+$y1 = 20 + 22;
 $y2 = $y1 + 10 + $text_height;
-$height = $y2 + 20 + 20/2;
+$height = $y2 + 20 + 22/2;
 
 $im = imagecreatetruecolor ($width, $height);
 imagealphablending($im, true);
@@ -36,8 +36,8 @@ imagesavealpha($im, true);
 $background = imagecolorallocate($im, 0, 0, 0);
 imagefill($im,0,0,0x7fff0000);
 $color = imagecolorallocate($im, 255, 255, 255);
-imagettftext($im, 20, 0, $x1, $y1, $color, '../uploaded_fonts/kgsevensixteen.ttf', $new_message1);
-imagettftext($im, 20, 0, $x2, $y2, $color, '../uploaded_fonts/kgsevensixteen.ttf', $new_message2);
+imagettftext($im, 22, 0, $x1, $y1, $color, '../uploaded_fonts/comingsoon.ttf', $new_message1);
+imagettftext($im, 22, 0, $x2, $y2, $color, '../uploaded_fonts/comingsoon.ttf', $new_message2);
 imagepng($im);
 imagedestroy($im);
 ?>
