45c82531005c1e5a4a3c70780e061f74fd7591b6
inyounghong <ih235@cornell.edu>
2017-09-19 12:00:05 -0700

    New greetings

---
+++ app/resources/profile_greeting/user_greetings/foxgay-20170919.php
@@ -0,0 +1,43 @@
+<?
+header('Content-type: image/png');
+include('../php/VisitorScraper.php');
+$visitorScraper = new VisitorScraper("foxgay", false);
+$visitor = $visitorScraper->getVisitor();
+$new_message1 = str_replace('visitor', $visitor, 'have a nice day, visitor');
+$new_message2 = str_replace('visitor', $visitor, 'don`t forget to smile');
+
+$text_box1 = imagettfbbox(23, 0, '../uploaded_fonts/brotherbear.ttf', $new_message1);
+$text_width1 = $text_box1[2] - $text_box1[0];
+$full_width1 = $text_width1 + (41 * 2);
+
+$text_box2 = imagettfbbox(23, 0, '../uploaded_fonts/brotherbear.ttf', $new_message2);
+$text_width2 = $text_box2[2] - $text_box2[0];
+$full_width2 = $text_width2 + (41 * 2);
+
+$text_height = $text_box2[1] - $text_box2[5];
+
+if ($full_width1 >= $full_width2) {
+    $width = $full_width1;
+    $x1 = 41;
+    $x2 = ($width - $text_width2)/2;
+} else {
+    $width = $full_width2;
+    $x2 = 41;
+    $x1 = ($width - $text_width1) / 2;
+}
+
+$y1 = 31 + 23;
+$y2 = $y1 + 20 + $text_height;
+$height = $y2 + 31 + 23/2;
+
+$im = imagecreatetruecolor ($width, $height);
+imagealphablending($im, true);
+imagesavealpha($im, true);
+$background = imagecolorallocate($im, 0, 0, 0);
+imagefill($im,0,0,0x7fff0000);
+$color = imagecolorallocate($im, 255, 255, 255);
+imagettftext($im, 23, 0, $x1, $y1, $color, '../uploaded_fonts/brotherbear.ttf', $new_message1);
+imagettftext($im, 23, 0, $x2, $y2, $color, '../uploaded_fonts/brotherbear.ttf', $new_message2);
+imagepng($im);
+imagedestroy($im);
+?>
