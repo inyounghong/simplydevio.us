8a970050ac7ebeb83694571b7b3fdf81e416cbda
inyounghong <ih235@cornell.edu>
2017-10-08 09:41:10 -0700

    Fix wanna project

--- work/wanna/README.md
+++ work/wanna/README.md
@@ -1,2 +1,4 @@
 # wanna
-A kanban-style want-to-do app in Javascript
+A beautiful, kanban-style want-to-do app in Javascript. <a href="http://www.simplydevio.us/work/wanna/index.php">View Live</a>
+
+<img src="screenshot.png">
--- work/wanna/app.php
+++ work/wanna/app.php
@@ -7,33 +7,24 @@
     <link rel="stylesheet" type="text/css" href="css/jquery.gridster.css">
     <link rel="stylesheet" type="text/css" href="css/app2.css">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Over+the+Rainbow' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 </head>
 
 <body>
     <?php include 'includes/menu.html' ?>
-    
-    <div id="background">
 
-    </div>
+    <div id="background"></div>
     <div class="wrap_again">
-    <div class="view_container">
-
-        <form>
-            <div id="title" class="title">
-            </div>
-        
-        <div id="grid" class="gridster">
-
-            <ul id="ul" class="ul">
-                
-            </ul>
+        <div class="view_container">
+            <form>
+                <div id="title" class="title"></div>
+                <div id="grid" class="gridster">
+                    <ul id="ul" class="ul">
+                    </ul>
+                </div>
+            </form>
         </div>
-        </form>
     </div>
-</div>
-
 
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/jquery.gridster.js" charset="utf-8"></script>
