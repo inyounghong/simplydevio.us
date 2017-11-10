c28bc269b3fd00490d7f0e4f6df18a74b2c6d2a8
inyounghong <ih235@cornell.edu>
2017-11-10 14:11:41 -0500

    update commit message

--- git-push-script.sh
+++ git-push-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/www
 git add .
-git commit -m "New greetings"
+git commit -m "Automatic update"
 git push https://inyounghong:Wendepunkt#7@github.com/inyounghong/SimplyDevio.us.git master
