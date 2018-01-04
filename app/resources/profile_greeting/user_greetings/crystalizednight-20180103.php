3c852e0bc087d48ab00aaca4eecee25fd24d5753
inyounghong <ih235@cornell.edu>
2018-01-04 00:00:04 -0700

    Automatic update

---
+++ app/resources/profile_greeting/user_greetings/crystalizednight-20180103.php
@@ -0,0 +1,43 @@
+<?
+header('Content-type: image/png');
+include('../php/VisitorScraper.php');
+$visitorScraper = new VisitorScraper("crystalizednight", false);
+$visitor = $visitorScraper->getVisitor();
+$new_message1 = str_replace('visitor', $visitor, 'Hey visitor!');
+$new_message2 = str_replace('visitor', $visitor, 'Welcome to my page!');
+
+$text_box1 = imagettfbbox(32, 0, '../uploaded_fonts/arberkley.ttf', $new_message1);
+$text_width1 = $text_box1[2] - $text_box1[0];
+$full_width1 = $text_width1 + (20 * 2);
+
+$text_box2 = imagettfbbox(32, 0, '../uploaded_fonts/arberkley.ttf', $new_message2);
+$text_width2 = $text_box2[2] - $text_box2[0];
+$full_width2 = $text_width2 + (20 * 2);
+
+$text_height = $text_box2[1] - $text_box2[5];
+
+if ($full_width1 >= $full_width2) {
+    $width = $full_width1;
+    $x1 = 20;
+    $x2 = ($width - $text_width2)/2;
+} else {
+    $width = $full_width2;
+    $x2 = 20;
+    $x1 = ($width - $text_width1) / 2;
+}
+
+$y1 = 20 + 32;
+$y2 = $y1 + 10 + $text_height;
+$height = $y2 + 20 + 32/2;
+
+$im = imagecreatetruecolor ($width, $height);
+imagealphablending($im, true);
+imagesavealpha($im, true);
+$background = imagecolorallocate($im, 197, 143, 255);
+imagefill($im,0,0,$background);
+$color = imagecolorallocate($im, 0, 0, 0);
+imagettftext($im, 32, 0, $x1, $y1, $color, '../uploaded_fonts/arberkley.ttf', $new_message1);
+imagettftext($im, 32, 0, $x2, $y2, $color, '../uploaded_fonts/arberkley.ttf', $new_message2);
+imagepng($im);
+imagedestroy($im);
+?>
