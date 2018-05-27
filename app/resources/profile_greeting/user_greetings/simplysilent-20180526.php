83927df544521bc00f6eef8d9989c1f5f5ca64b2
inyounghong <ih235@cornell.edu>
2018-05-27 00:00:02 -0700

    Automatic update

---
+++ app/resources/profile_greeting/user_greetings/simplysilent-20180526.php
@@ -0,0 +1,43 @@
+<?
+header('Content-type: image/png');
+include('../php/VisitorScraper.php');
+$visitorScraper = new VisitorScraper("simplysilent", false);
+$visitor = $visitorScraper->getVisitor();
+$new_message1 = str_replace('visitor', $visitor, '');
+$new_message2 = str_replace('visitor', $visitor, 'you\'re safe with me <3');
+
+$text_box1 = imagettfbbox(33, 0, '../uploaded_fonts/determinationsansweb-webfont.ttf', $new_message1);
+$text_width1 = $text_box1[2] - $text_box1[0];
+$full_width1 = $text_width1 + (59 * 2);
+
+$text_box2 = imagettfbbox(33, 0, '../uploaded_fonts/determinationsansweb-webfont.ttf', $new_message2);
+$text_width2 = $text_box2[2] - $text_box2[0];
+$full_width2 = $text_width2 + (59 * 2);
+
+$text_height = $text_box2[1] - $text_box2[5];
+
+if ($full_width1 >= $full_width2) {
+    $width = $full_width1;
+    $x1 = 59;
+    $x2 = ($width - $text_width2)/2;
+} else {
+    $width = $full_width2;
+    $x2 = 59;
+    $x1 = ($width - $text_width1) / 2;
+}
+
+$y1 = 20 + 33;
+$y2 = $y1 + 0 + $text_height;
+$height = $y2 + 20 + 33/2;
+
+$im = imagecreatetruecolor ($width, $height);
+imagealphablending($im, true);
+imagesavealpha($im, true);
+$background = imagecolorallocate($im, 3, 3, 255);
+imagefill($im,0,0,$background);
+$color = imagecolorallocate($im, 255, 255, 255);
+imagettftext($im, 33, 0, $x1, $y1, $color, '../uploaded_fonts/determinationsansweb-webfont.ttf', $new_message1);
+imagettftext($im, 33, 0, $x2, $y2, $color, '../uploaded_fonts/determinationsansweb-webfont.ttf', $new_message2);
+imagepng($im);
+imagedestroy($im);
+?>
